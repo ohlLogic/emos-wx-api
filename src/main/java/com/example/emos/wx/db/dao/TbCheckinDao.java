cb05e474147431aea9233ed16a1ea7f35baf94b1
Logic <ohlself@gmail.com>
2022-07-18 21:13:59 +0800

    签到（持久层）

--- src/main/java/com/example/emos/wx/db/dao/TbCheckinDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.emos.wx.db.dao;
-
-import com.example.emos.wx.db.pojo.TbCheckin;
-
-public interface TbCheckinDao {
-
-}
--- src/main/java/com/example/emos/wx/db/dao/TbHolidaysDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.emos.wx.db.dao;
-
-import com.example.emos.wx.db.pojo.TbHolidays;
-
-public interface TbHolidaysDao {
-
-}
