09d05c1318a84c651168b5ff8166b9045f5e6eb9
Logic <ohlself@gmail.com>
2022-07-23 15:23:36 +0800

    人脸签到（业务层）

--- src/main/java/com/example/emos/wx/service/impl/CheckinServiceImpl.java
+++ src/main/java/com/example/emos/wx/service/impl/CheckinServiceImpl.java
@@ -2,16 +2,24 @@ package com.example.emos.wx.service.impl;
 
 import cn.hutool.core.date.DateTime;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import cn.hutool.http.HttpUtil;
 import com.example.emos.wx.config.SystemConstants;
 import com.example.emos.wx.db.dao.TbCheckinDao;
+import com.example.emos.wx.db.dao.TbFaceModelDao;
 import com.example.emos.wx.db.dao.TbHolidaysDao;
 import com.example.emos.wx.db.dao.TbWorkdayDao;
+import com.example.emos.wx.exception.EmosException;
 import com.example.emos.wx.service.CheckinService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.HashMap;
 
 @Service
@@ -19,7 +27,7 @@ import java.util.HashMap;
 @Slf4j
 public class CheckinServiceImpl implements CheckinService {
     @Autowired
-    private SystemConstants systemConstants;
+    private SystemConstants constants;
 
     @Autowired
     private TbHolidaysDao holidaysDao;
@@ -29,6 +37,15 @@ public class CheckinServiceImpl implements CheckinService {
 
     @Autowired
     private TbCheckinDao checkinDao;
+
+    @Autowired
+    private TbFaceModelDao faceModelDao;
+
+    @Value("${emos.face.createFaceModelUrl}")
+    private String createFaceModelUrl;
+
+    @Value("${emos.face.checkinUrl}")
+    private String checkinUrl;
 
     @Override
     public String validCanCheckIn(int userId, String date) {
@@ -48,8 +65,8 @@ public class CheckinServiceImpl implements CheckinService {
             return "节假日不需要考勤";
         } else {
             DateTime now = DateUtil.date();
-            String start = DateUtil.today() + " " + systemConstants.attendanceStartTime;
-            String end = DateUtil.today() + " " + systemConstants.attendanceEndTime;
+            String start = DateUtil.today() + " " + constants.attendanceStartTime;
+            String end = DateUtil.today() + " " + constants.attendanceEndTime;
             DateTime attendanceStart = DateUtil.parse(start);
             DateTime attendanceEnd = DateUtil.parse(end);
             if (now.isBefore(attendanceStart)) {
@@ -64,6 +81,54 @@ public class CheckinServiceImpl implements CheckinService {
                 map.put("end", end);
                 boolean bool = checkinDao.haveCheckin(map) != null ? true : false;
                 return bool ? "今日已经考勤，不用重复考勤" : "可以考勤";
+            }
+        }
+    }
+
+    @Override
+    public void checkin(HashMap param) {
+        Date d1 = DateUtil.date();
+        Date d2 = DateUtil.parse(DateUtil.today() + " " + constants.attendanceTime);
+        Date d3 = DateUtil.parse(DateUtil.today() + " " + constants.attendanceEndTime);
+        int status = 1;
+        if(d1.compareTo(d2) <= 0)
+        {
+            status = 1; // 正常
+        }
+        else if(d1.compareTo(d2) > 0 && d1.compareTo(d3) < 0)
+        {
+            status = 2; // 迟到
+        }
+
+        int userId = (int) param.get("userId");
+        String faceModel = faceModelDao.searchFaceModel(userId);
+        if(faceModel == null)
+        {
+            throw new EmosException("不存在人脸模型");
+        }
+        else{
+            String path = (String) param.get("path");
+            HttpRequest request = HttpUtil.createPost(checkinUrl);
+            request.form("photo", FileUtil.file(path), "targetModel", faceModel);
+            HttpResponse response = request.execute();
+            // 判断返回响应码
+            if(response.getStatus() != 200)
+            {
+                log.error("人脸识别服务异常");
+                throw new EmosException("人脸识别服务异常");
+            }
+            String body = response.body();
+            if("无法识别出人脸".equals(body) || "照片中存在多张人脸".equals(body))
+            {
+                throw new EmosException(body);
+            }
+            else if("False".equals(body)){
+                throw new EmosException("签到无效，非本人签到");
+            }
+            else if("True".equals(body))
+            {
+                // TODO 查询疫情风险等级
+                // TODO 保存签到记录
             }
         }
     }
